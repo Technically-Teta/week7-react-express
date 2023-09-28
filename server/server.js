594ca9fa4c23648a983247131fe3d2927f2048fe
tushort <samquioh@gmail.com>
2023-09-27 21:24:48 -0700

    adds clone for new repo here

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+
+const PORT = 8080;
+app.use(cors());
+app.use(express.json());
+
+// creates an endpoint for the route /api
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from my backend. This is my weather app part 2 '});
+  });
+
+// creates an endpoint for the route /api/weather
+app.get('/api/weather', (req, res) => {
+  res.json(dataWeather);
+});
+
+// console.log that your server is up and running
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+
+  
